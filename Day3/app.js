dfbe71769f1d22463c0b0bce1e6ce5e1b3e08556
Abhilash S Naik <129758286+abhi-chethu@users.noreply.github.com>
2023-10-19 18:52:09 +0530

    Create app.js

---
+++ Day3/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Cardlist from './CardList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bootstrap Cards in React</h1>
+      <Cardlist />
+    </div>
+  );
+}
+
+export default App;
